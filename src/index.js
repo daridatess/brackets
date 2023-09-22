61c86827f85960583a50005e0bf2547ef4dbe868
godandy123@gmail.com <godandy123@gmail.com>
2023-09-23 00:02:32 +0300

    feat: submit task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
-module.exports = function check(str, bracketsConfig) {};
+module.exports = function check(str, bracketsConfig) {
+  const openBracket = [];
+  const closeBracket = [];
+  let indexOpenBreack = []
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openBracket.push(bracketsConfig[i][0]);
+    closeBracket.push(bracketsConfig[i][1]);
+  }
+
+  const checkString = str.split('')
+for (let i = 0; i < checkString.length; i++) {
+  if(openBracket.includes(checkString[i])) {
+    if(openBracket.indexOf(checkString[i]) == closeBracket.indexOf(checkString[i])){
+      if(indexOpenBreack[indexOpenBreack.length - 1] != closeBracket.indexOf(checkString[i]) ){
+         indexOpenBreack.push(openBracket.indexOf(checkString[i]))
+      } else {
+        if(indexOpenBreack.pop() != closeBracket.indexOf(checkString[i])) {
+          return false
+        }
+      }
+    } else {
+  
+      indexOpenBreack.push(openBracket.indexOf(checkString[i]))
+    }
+  } else if (closeBracket.includes(checkString[i])) {
+    if(indexOpenBreack.pop() != closeBracket.indexOf(checkString[i])) {
+return false
+    }
+  }
+}
+
+if(indexOpenBreack.length) {
+  return false
+}
+return true
+}
 
 const config1 = [['(', ')']];
 const config2 = [
